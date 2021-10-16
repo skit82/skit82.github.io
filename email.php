0c8c13ea7d56cb3ab85018b2aece0ad071ebac87
skit82 <a.abrosimow2012@yandex.ru>
2021-10-16 14:12:29 +0300

    Add files via upload

--- email.php
+++ email.php
@@ -1,10 +1,31 @@
  <?php
-$to = "sertamaster23@yandex.ru"; // емайл получателя данных из формы
-$tema = "Заявка на сотрудничество"; // тема полученного емайла
-$message = "Ваше имя: ".$_POST['callform-login']."<br>";//присвоить переменной значение, полученное из формы name=name
-$message .= "E-mail: ".$_POST['callform-email']."<br>"; //полученное из формы name=email
-$message .= "Номер телефона: ".$_POST['callform-tel']."<br>"; //полученное из формы name=phone
-$headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
-$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
-mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
+
+ 	$name = $_POST['callform-login'];
+ 	$tel = $_POST['callform-tel'];
+ 	$email = $_POST['callform-email'];
+
+ 	$name = htmlspecialchars($name);
+ 	$tel = htmlspecialchars($tel);
+ 	$email = htmlspecialchars($email);
+
+ 	$name = urldecode($name);
+ 	$tel = urldecode($tel);
+ 	$email = urldecode($email);
+
+ 	$name = trim($name);
+ 	$tel = trim($tel);
+ 	$email = trim($email);
+
+ 	if (mail('sertamaster23@yandex.ru',
+ 			'Заявка на соотрудничество',
+ 			'Имя: '.$name.'\n'.
+ 			'Телефон: '.$tel.'\n'.
+ 			'Email: '.$email.'\n'.)
+ 	){
+ 		echo ('Письмо успешно отправлено!');
+ 	}
+
+ 	else {
+ 		echo('Есть ошибки! Проверьте данные...');
+ 	}
 ?>
